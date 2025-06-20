a586b1224b7431bdc2decb9ccc62c3b3b301f043
Paul Castaldo <pauldcastaldo@gmail.com>
2025-06-20 17:23:22 -0500

    added 'example_item' folder containing the example item's project folder

---
+++ example_item/CustomParamTest_Necrootmancy/obj/Debug/netstandard2/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Template
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Necrootmancy";
+        public const string PLUGIN_NAME = "Necrootmancy";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
